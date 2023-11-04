36d339448b1634165cbce0819938342fa592be32
seabasscosta <149906426+seabasscosta@users.noreply.github.com>
2023-11-04 22:11:54 +0100

    Add files via upload

---
+++ my.js
@@ -0,0 +1,16 @@
+function testGS (){
+
+const url = "https://script.google.com/macros/s/AKfycbxXEa7db2KJEfMrKmqbZ2R_1S-mU6PKyFpCUVxAfJFH0cTT1dkTKD5GNBRO60iY5Nk7/exec"
+
+fetch(url).
+then(d => d.json())
+.then(d => {
+
+	d[0].status;
+		document.getElementById("app").textContent = d[0].status;
+
+});
+
+}
+
+document.getElementById("btn").addEventListener("click",testGS);
